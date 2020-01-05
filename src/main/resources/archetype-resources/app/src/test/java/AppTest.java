3b50ba0bc8405a07b0c58739436b358af72a418c
Ruwen Reddig <ruwen.reddig@gmail.com>
2020-01-05 10:17:41 +0100

    initial commit

---
+++ src/main/resources/archetype-resources/app/src/test/java/AppTest.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
